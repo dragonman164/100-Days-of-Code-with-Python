e396f28bcac1d792f41b10297411c85d2a15810b
dragonman164 <sanidhiyafirefox123@gmail.com>
2023-11-28 21:46:43 +0530

    2nd day code

---
+++ 2.py
@@ -0,0 +1,28 @@
+# 1. Printing on Console 
+# 2. How to take input from Console 
+# 3. Comments 
+
+"""
+fhsfhsdjkf h
+dsfhsdfjh sdf
+dsfhsdfjh sdkjf
+"""
+
+# print(1)
+# print("Hello World")
+# print("Sanidhiya")
+
+# print(1 + 2)
+# print(1*3)
+# print(1 - 3)
+
+# print("asas",end = " ")
+# print("asasas")
+
+a = input("What is your name?")
+print(a)
+
+# String Integer 
+a = int(input("Enter your first no."))
+b = int(input("Enter your seccond no."))
+print(a + b)
